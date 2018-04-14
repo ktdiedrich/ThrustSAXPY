05789c052bb257b0c9558c0838ad776f49aa385d
Karl Diedrich <ktdiedrich@gmail.com>
2018-04-14 10:47:35 -0400

    SAXPY algorithm implemented on GPU. License. Compile instructions.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # ThrustSAXPY
+
 SAXPY operation: y &lt;-a * x + y x = vector, y = vector, a = scalar constant ; on GPU
+
+@author Karl Diedrich, PhD ktdiedrich@gmail.com
+
+## Formula
+
+SAXPY operation y <-a * x + y
+x = vector, y = vector, a = scalar constant
+
+## Implementation
+
+Based on https://github.com/thrust/thrust/wiki/Quick-Start-Guide
+
+## Project
+
+Started with Visual Studio CUDA template
+
+## Compile
+
+### Windows compile
+
+VS 2017 compiler not always compatible with CUDA toolkit
+Project -> PROJECT_NAME properties -> Configuration Properties-> general -> Platform toolset: choose Visual Studio 2015 (v140)
+Project-> PROJECT_NAME properties -> Configuration Properties -> CUDA C/C++ -> Command Line -> Additional Options: --expt-extended-lambda  
+
+Command line compile:
+1>...\ThrustSAXPY\ThrustSAXPY>"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin\nvcc.exe" -ccbin "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64" -x cu  -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\include" -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\include"  -G   --keep-dir x64\Debug -maxrregcount=0  --machine 64 --compile  --expt-extended-lambda  -g   -DWIN32 -DWIN64 -D_DEBUG -D_CONSOLE -D_MBCS -Xcompiler "/EHsc /W3 /nologo /Od /FS /Zi /RTC1 /MDd " -o x64\Debug\kernel.cu.obj "...\ThrustSAXPY\ThrustSAXPY\kernel.cu" -clean
+1>kernel.cu
+1>Compiling CUDA source file kernel.cu...
+1>
+1>...\ThrustSAXPY\ThrustSAXPY>"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin\nvcc.exe" -gencode=arch=compute_30,code=\"sm_30,compute_30\" --use-local-env --cl-version 2015 -ccbin "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64" -x cu  -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\include" -I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\include"  -G   --keep-dir x64\Debug -maxrregcount=0  --machine 64 --compile -cudart static --expt-extended-lambda -g   -DWIN32 -DWIN64 -D_DEBUG -D_CONSOLE -D_MBCS -Xcompiler "/EHsc /W3 /nologo /Od /FS /Zi /RTC1 /MDd " -o x64\Debug\kernel.cu.obj "...\ThrustSAXPY\ThrustSAXPY\kernel.cu"
+
+
+### Linux compile
+
+see ThrustSAXPY/compile.sh script
+
+## License
+
+/*=========================================================================
+*
+*  Copyright (c) 2017  Karl T. Diedrich, PhD
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
---
+++ ThrustSAXPY/kernel.cu
@@ -0,0 +1,139 @@
+/*=========================================================================
+*
+*  Copyright (c) 2017  Karl T. Diedrich, PhD
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+#include <thrust/transform.h>
+#include <thrust/replace.h>
+#include <thrust/functional.h>
+
+#include <thrust/copy.h>
+#include <thrust/fill.h>
+#include <thrust/sequence.h>
+#include <thrust/count.h>
+
+#include <iostream>  
+#include <list>
+#include <vector>
+
+
+/** SAXPY implementation 
+SAXPY operation y <-a * x + y
+x = vector, y = vector, a = scalar constant
+
+Based on https://github.com/thrust/thrust/wiki/Quick-Start-Guide 
+
+
+@author Karl Diedrich, PhD ktdiedrich@gmail.com
+*/ 
+
+/* Simplify names with aliases */
+typedef float CalcNumber;
+typedef thrust::device_vector<CalcNumber> DeviceNumVector;
+
+// Function definitions, can be in header file. In one file for simplicity of sample project. 
+
+/** Fast SAXPY operator with lambda call
+Lambda implementation requires nvcc compile option: --expt-extended-lambda */
+void saxpy_fast(CalcNumber A, DeviceNumVector & X, DeviceNumVector & Y);
+
+
+/** SAXPY y <-a * x + y in steps  */
+void saxpy_slow(CalcNumber A, DeviceNumVector & X, DeviceNumVector & Y);
+
+/** Test example SAXPY operation y <- a * x + y
+x = vector, y = vector, a = scalar constant
+*/
+void testSaxpy();
+
+/** */
+void testReduce();
+
+int main()
+{
+	testSaxpy();
+	testReduce();
+    return 0;
+}
+
+
+// Function implementations 
+void saxpy_fast(CalcNumber A, DeviceNumVector & X, DeviceNumVector & Y)
+{
+	// auto saxpy_lambda = [=] __device__(auto x, auto y) { return A * x + y; };
+	auto saxpy_lambda = [=] __device__(CalcNumber x, CalcNumber y) { return A * x + y; };
+	thrust::transform(X.begin(), X.end(), Y.begin(), Y.begin(), saxpy_lambda);
+}
+
+void saxpy_slow(CalcNumber A, DeviceNumVector & X, DeviceNumVector & Y)
+{
+	DeviceNumVector temp(X.size());
+	// temp <- A 
+	thrust::fill(temp.begin(), temp.end(), A);
+	// temp <- A * x
+	thrust::transform(X.begin(), X.end(), temp.begin(), temp.begin(), thrust::multiplies<CalcNumber>());
+	// Y <- A * X + Y
+	thrust::transform(temp.begin(), temp.end(), Y.begin(), Y.begin(), thrust::plus<CalcNumber>());
+}
+
+
+void testSaxpy()
+{
+	// CalcNumber expected[] = { 3,  5,  7,  9, 11, 13, 15, 17, 19, 21 };
+	CalcNumber const a1 = 2.0;
+	CalcNumber const addY = 4.0;
+	int const VECTOR_SIZE = 10;
+	DeviceNumVector x1(VECTOR_SIZE);
+	DeviceNumVector y1(VECTOR_SIZE);
+	thrust::sequence(x1.begin(), x1.end());
+	thrust::fill(y1.begin(), y1.end(), addY);
+	std::cout << "SAXPY test: Y <- A*X+Y\na1 <-" << a1 << std::endl;
+	std::cout << "x1 <- ";
+	thrust::copy(x1.begin(), x1.end(), std::ostream_iterator<CalcNumber>(std::cout, ", "));
+	std::cout << std::endl;
+
+	std::cout << "y1 <- ";
+	thrust::copy(y1.begin(), y1.end(), std::ostream_iterator<CalcNumber>(std::cout, ", "));
+	std::cout << std::endl;
+	saxpy_fast(a1, x1, y1);
+	// saxpy_slow(a1, x1, y1);
+	std::cout << "y1 transformed: ";
+	thrust::copy(y1.begin(), y1.end(), std::ostream_iterator<CalcNumber>(std::cout, ", "));
+	std::cout << std::endl;
+}
+
+void testReduce()
+{
+	DeviceNumVector D(10);
+	thrust::sequence(D.begin(), D.end());
+	std::cout << "Reduce: ";
+	thrust::copy(D.begin(), D.end(), std::ostream_iterator<CalcNumber>(std::cout, ", "));
+	auto calcSum = thrust::reduce(D.begin(), D.end(), static_cast<CalcNumber>(0), thrust::plus<CalcNumber>());
+	std::cout << "sum = " << calcSum << std::endl;
+
+	thrust::device_vector<int> vec(6, 0);
+	vec[0] = 1; vec[1] = 2; vec[2] = 2;  vec[3] = 3; vec[4] = 1;
+	std::cout << "Vector: ";
+	thrust::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, ", "));
+	int ones = thrust::count(vec.begin(), vec.end(), 1);
+	std::cout << "Ones = " << ones << std::endl;
+}
