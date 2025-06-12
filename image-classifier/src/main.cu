4435dceb3220daac92ef13915084dde4522ec41c
Karl Diedrich <ktdiedrich@gmail.com>
2025-06-11 21:47:30 -0700

    Use descriptive names for data types

--- image-classifier/src/main.cu
+++ image-classifier/src/main.cu
@@ -30,31 +30,31 @@ int main(int argc, char** argv) {
         }
         std::cout << " word_size=" << array.word_size;
     
-        std::vector<std::vector<uint8_t>> vec2d;
-        std::vector<std::vector<std::vector<uint8_t>>> vec3d;
+        std::vector<std::vector<uint8_t>> one_hot_labels2d;
+        std::vector<std::vector<std::vector<uint8_t>>> list_image;
         try {
-            load_array_to_vectors<uint8_t>(array, vec2d, vec3d);
+            load_array_to_vectors<uint8_t>(array, one_hot_labels2d, list_image);
             // Example: print first value if available
-            if (!vec2d.empty() && !vec2d[0].empty()) { 
+            if (!one_hot_labels2d.empty() && !one_hot_labels2d[0].empty()) { 
                 std::cout << " Label vec2d[0]=";
-                for (size_t j = 0; j < vec2d[0].size(); ++j) {
-                    std::cout << static_cast<int>(vec2d[0][j]) << " ";
+                for (size_t j = 0; j < one_hot_labels2d[0].size(); ++j) {
+                    std::cout << static_cast<int>(one_hot_labels2d[0][j]) << " ";
                 }
-                auto label_histogram = one_hot_histogram<uint8_t>(vec2d);
+                auto label_histogram = one_hot_histogram<uint8_t>(one_hot_labels2d);
                 std::cout << "\n1-hot hist: ";
                 for (const auto& kv : label_histogram) {
                     std::cout << kv.first << ":" << kv.second << " | ";
                 }
                 std::cout << std::endl;
-                print_one_hot_histogram_with_labels<uint8_t>(vec2d, CHEST_LABELS);
+                print_one_hot_histogram_with_labels<uint8_t>(one_hot_labels2d, CHEST_LABELS);
                 plot_histogram_to_image(label_histogram, CHEST_LABELS, array_name + "_hist.png");
             } else {
                 std::cout << " No 2D data available.";  
             }
-            if (!vec3d.empty() && !vec3d[0].empty() && !vec3d[0][0].empty()) {
-                std::cout << " First 3D value=" << static_cast<int>(vec3d[0][0][0]);
+            if (!list_image.empty() && !list_image[0].empty() && !list_image[0][0].empty()) {
+                std::cout << " First 3D value=" << static_cast<int>(list_image[0][0][0]);
                 const int slice_number = 0;
-                write_image<uint8_t>(array_name, vec3d[slice_number], slice_number, CV_8UC1, "png");
+                write_image<uint8_t>(array_name, list_image[slice_number], slice_number, CV_8UC1, "png");
             }
         } catch (const std::exception& ex) {
             std::cerr << "Error loading array: " << ex.what() << std::endl;
