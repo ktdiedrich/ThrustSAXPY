591c9ef4db149ef11dd3276684a2059655c97e86
Karl Diedrich <ktdiedrich@gmail.com>
2025-06-13 23:37:22 -0700

    Descriptive names

--- image-classifier/src/main.cu
+++ image-classifier/src/main.cu
@@ -16,11 +16,11 @@ int main(int argc, char** argv) {
     }
     std::map<std::string, cnpy::NpyArray> all_arrays = read_all_npz_arrays(argv[1]);
 
-    std::map<std::string, std::vector<std::vector<data_type_t>>> arrays_2d;
-    std::map<std::string, std::vector<std::vector<std::vector<data_type_t>>>> arrays_3d;
-    std::map<std::string, std::vector<thrust::device_vector<data_type_t>>> device_arrays_2d;
-    std::map<std::string, std::vector<thrust::device_vector<data_type_t>>> device_arrays_3d;
-    std::tie(arrays_2d, arrays_3d, device_arrays_2d, device_arrays_3d) = get_vector_maps(all_arrays);
+    std::map<std::string, std::vector<std::vector<data_type_t>>> one_hot_labels_2d;
+    std::map<std::string, std::vector<std::vector<std::vector<data_type_t>>>> images_3d;
+    std::map<std::string, std::vector<thrust::device_vector<data_type_t>>> device_one_hot_labels_2d;
+    std::map<std::string, std::vector<thrust::device_vector<data_type_t>>> device_images_3d;
+    std::tie(one_hot_labels_2d, images_3d, device_one_hot_labels_2d, device_images_3d) = get_vector_maps(all_arrays);
     
     return 0;
 }
